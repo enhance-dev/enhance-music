cf1e90deb74cb735da0dced5f315e6776628701b
Cole Peters <cole@colepeters.com>
2023-08-23 15:59:08 -0700

    Position player iframe

--- app/elements/audio-player.mjs
+++ app/elements/audio-player.mjs
@@ -3,7 +3,14 @@ export default function AudioPlayer ({ html, state }) {
   const { track } = store
 
   return html`
-    <figure class='flex justify-content-center'>
+    <style scope='global'>
+      /* Targets iframe's document */
+      html,
+      body {
+        block-size: 100%;
+      }
+    </style>
+    <figure class='flex justify-content-center align-items-center sb-100'>
       <audio autoplay controls src='${track}'></audio>
     </figure>
   `
--- app/elements/layout-grid.mjs
+++ app/elements/layout-grid.mjs
@@ -5,6 +5,16 @@ export default function LayoutGrid ({ html }) {
         display: grid;
         grid-template-rows: auto 1fr;
         height: 100vh;
+        position: relative;
+      }
+
+      [name="library"] {
+        padding-block-end: var(--playerHeight);
+      }
+
+      [name="player"] {
+        border-block-start: 1px solid gainsboro;
+        block-size: var(--playerHeight);
       }
     </style>
     <slot></slot>
