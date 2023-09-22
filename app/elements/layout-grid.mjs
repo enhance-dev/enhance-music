afc93ce40cd2df0628f350c910233fdcdf1d1845
Cole Peters <cole@colepeters.com>
2023-09-22 14:46:18 -0500

    Revise layout grid for dynamic `display` of player iframe

--- app/elements/layout-grid.mjs
+++ app/elements/layout-grid.mjs
@@ -3,7 +3,8 @@ export default function LayoutGrid ({ html }) {
     <style>
       :host {
         display: grid;
-        grid-template-rows: auto 1fr var(--playerHeight);
+        grid-template-rows: auto 1fr;
+        grid-auto-rows: var(--playerHeight);
         block-size: 100vh;
         overflow: hidden;
       }
