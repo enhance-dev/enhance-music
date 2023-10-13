c2a528c7d359269eb59aee6d8549f0ed7d1b53d3
Jonathan Lipps <jlipps@gmail.com>
2023-10-13 11:52:08 -0700

    Add some of @jlipps albums

--- app/api/player/$id.mjs
+++ app/api/player/$id.mjs
@@ -5,9 +5,12 @@ export const get = [async function get (req) {
   const { session: prevSession } = req
   const { id } = req.pathParameters
 
-  // `id` formatted as 'N1-N2' where N1 is the single digit album ID,
-  // and N2 is the single digit track number
-  const [albumId, , trackId] = id
+  // `id` formatted as 'N1-N2' where N1 is the numeric album ID,
+  // and N2 is the numeric track number
+  const [, albumId, trackId] = /([0-9]+)-([0-9]+)/.exec(id)
+  if (!albumId || !trackId) {
+    throw new Error(`Id '${id}' was not of the format <albumNum>-<trackNum>`)
+  }
 
   const album = albums.find(album => album.id == albumId)
   const track = album?.tracklist[trackId - 1]
